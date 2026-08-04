9439fe23f62818fc6c6b05fd015804d48c61b832
mnayyab01-boopb <mnayyab01@gmail.com>
2026-08-04 14:48:17 +0500

    Day 20: Task Manager API Development

---
+++ Day20TaskManagerApi/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagerApi.Models;
+namespace TaskManagerApi.Data;
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<TaskItem> TaskItems { get; set; }
+}
---
+++ Day20TaskManagerApi/Models/TaskItem.cs
@@ -0,0 +1,9 @@
+namespace TaskManagerApi.Models;
+public class TaskItem
+{
+    public int Id {get;set;}
+    public string Title {get;set;} =string.Empty;
+    public string Description {get;set;} =string.Empty;
+    public bool IsDone {get;set;}
+    public DateTime DuaDate {get;set;}
+}
