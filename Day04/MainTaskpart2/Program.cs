514d9312ab65188aad8f67f6a3899d865a0f217c
mnayyab01-boopb <mnayyab01@gmail.com>
2026-08-04 14:30:08 +0500

    Day 04: Methods and Arrays

---
+++ Day04/MainTaskpart2/Program.cs
@@ -0,0 +1,41 @@
+﻿List<int> numbers = new List<int>()
+{
+ 15, 25, 35, 45, 55
+};
+Findmax(numbers);
+Findmin(numbers);
+Findaverage(numbers);
+void Findmax(List<int> numbers)
+{
+int max = numbers[0];
+for (int i = 1; i < numbers.Count; i++)
+{
+if (numbers[i] > max)
+{
+max = numbers[i];
+}
+}
+Console.WriteLine("Maximum = " + max);
+}
+void Findmin(List<int> numbers)
+{
+int min = numbers[0];
+for (int i = 1; i < numbers.Count; i++)
+    {
+         if (numbers[i] < min)
+        {
+            min = numbers[i];
+        }
+    }
+ Console.WriteLine("Minimum = " + min);
+}
+void Findaverage(List<int> numbers)
+{
+int total = 0;
+for (int i = 0; i < numbers.Count; i++)
+{
+total = total + numbers[i];
+}
+double average = (double)total / numbers.Count;
+Console.WriteLine("Average = " + average);
+}
---
+++ Day04/Practice02array/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] number= new int[5];
+{
+    Console.WriteLine("Enter a  First Number");
+    number[0]=Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter a Second Number");
+    number[1]=Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter a third Number");
+    number[2]=Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter a four Number");
+    number[3]=Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter your Fifth Number");
+    number[4]=Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine(number[0]);
+Console.WriteLine(number[1]);
+Console.WriteLine(number[2]);
+Console.WriteLine(number[3]);
+Console.WriteLine(number[4]);
