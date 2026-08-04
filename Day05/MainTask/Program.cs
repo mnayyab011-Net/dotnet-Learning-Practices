99eee09b764fd594c9ecd4f9f6de2c4906637284
mnayyab01-boopb <mnayyab01@gmail.com>
2026-08-04 14:31:10 +0500

    Day 05: Object Oriented Programming Basics

---
+++ Day05/MainTask/Program.cs
@@ -0,0 +1,31 @@
+﻿students s1 = new students();
+s1.Name = "Ali";
+s1.Age = 26;
+s1.RN = 10;
+students s2 = new students();
+s2.Name = "Nayyab";
+s2.Age = 23;
+s2.RN = 12;
+students s3 = new students();
+s3.Name = "Muqadas";
+s3.Age = 23;
+s3.RN = 20;
+s1.GetDetails();
+Console.WriteLine();
+s2.GetDetails();
+Console.WriteLine();
+s3.GetDetails();
+Console.WriteLine();
+class students
+{
+    public string Name;
+    public int Age;
+    public int RN;
+
+    public void GetDetails()
+    {
+        Console.WriteLine("Student Name is: " + Name);
+        Console.WriteLine("Student Age is: " + Age);
+        Console.WriteLine("Student Roll Number is: " + RN);
+    }
+}
---
+++ Day05/Practices01/Program.cs
@@ -0,0 +1,17 @@
+﻿car car = new car();
+car.Start();
+car.Preparing();
+class Vehicle
+{
+    public void Start()
+    {
+        Console.WriteLine("Vehicle is Starting");
+    }
+}
+class car : Vehicle
+{
+    public void Preparing()
+    {
+        Console.WriteLine("Car is Preparing");
+    }
+}
