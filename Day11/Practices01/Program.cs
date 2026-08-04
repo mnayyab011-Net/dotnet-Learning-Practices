fc902edf33be6977c5252312b7789cbc6f4a1ec3
mnayyab01-boopb <mnayyab01@gmail.com>
2026-08-04 14:34:43 +0500

    Day 11: LINQ Basics

---
+++ Day11/Practices01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+class Program
+{
+static void Main(string[] args)
+    {
+        List<int> students = new List<int>
+        {
+            1,2,3,4,5
+        };
+        var result = students.Where(x => x > 2);
+        foreach(var number in result)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
