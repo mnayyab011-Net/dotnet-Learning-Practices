904903f7f8ce2483ce51fb37b49436a840c71a06
mnayyab01-boopb <mnayyab01@gmail.com>
2026-08-04 14:35:00 +0500

    Day 12: Async Programming Basics

---
+++ Day12/Practices01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+class Program
+{
+    static async Task Main()
+    {
+        Console.WriteLine("order Piza");
+        Task PizaTask=MakePizaAsync();
+        Console.WriteLine("start using Mobile Phone");
+        await PizaTask;
+        Console.WriteLine("order Arrived");
+    }
+    static async Task MakePizaAsync ()
+    {
+        Console.WriteLine("Chief Making Piza");
+        await Task.Delay(3000);
+        Console.WriteLine("Piza Ready");
+    }
+}
