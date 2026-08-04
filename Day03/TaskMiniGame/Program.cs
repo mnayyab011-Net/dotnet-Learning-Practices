6c09a4fd94a9bb3a1e904e23fff0d194e31470eb
mnayyab01-boopb <mnayyab01@gmail.com>
2026-08-04 14:29:39 +0500

    Day 03: Control Flow and Loops

---
+++ Day03/TaskMiniGame/Program.cs
@@ -0,0 +1,28 @@
+﻿int number=5;
+int attempt=0;
+int gnumber=0;
+while (number != gnumber && attempt<3)
+{
+  Console.WriteLine("Enter the number:");
+ gnumber = Convert.ToInt32(Console.ReadLine());
+ attempt++;
+   if (gnumber == number)
+ {
+  Console.WriteLine("you guess correct number");
+  break;
+ }
+ else if (gnumber > number)
+ {
+  Console.WriteLine("Number is graeter");
+ }
+ else 
+ {
+    Console.WriteLine("Enter number is low ");
+  }
+}
+  if (number != gnumber)
+{
+  Console.WriteLine("Maximum attempt");
+  Console.WriteLine("The correct number was: " + number);
+}
+
