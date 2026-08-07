5e92fcb2b117f4e94054a897216b1d8525aaab2a
mnayyab011-Net <nayyabmazhar8@gmail.com>
2026-08-07 15:16:03 +0500

    Delete Day15RestaurantsApi directory

--- Day15RestaurantsApi/Settings/MongoDBSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace RestaurantsApi.Settings;
-
-public class MongoDBSettings
-{
-    public string ConnectionString { get; set; } = "";
-    public string DatabaseName { get; set; } = "";
-    public string CollectionName { get; set; } = "";
-}
