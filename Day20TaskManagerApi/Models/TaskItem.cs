40a7048c5167a601d77e62dc23bd8f7517ca0e07
mnayyab011-Net <nayyabmazhar8@gmail.com>
2026-08-07 15:35:05 +0500

    Delete Day20TaskManagerApi directory

--- Day20TaskManagerApi/Models/TaskItem.cs
+++
@@ -1,9 +0,0 @@
-namespace TaskManagerApi.Models;
-public class TaskItem
-{
-    public int Id {get;set;}
-    public string Title {get;set;} =string.Empty;
-    public string Description {get;set;} =string.Empty;
-    public bool IsDone {get;set;}
-    public DateTime DuaDate {get;set;}
-}
