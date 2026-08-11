da853526451da42d0571c37b1d37b2444f7c51a0
mnayyab01-boopb <mnayyab01@gmail.com>
2026-08-11 16:53:47 +0500

    Day 3 - Control Flow and Number Guessing Game

---
+++ Day03/Practice04/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter your age");
+int age=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Do you have Cnic:");
+string cnic = Convert.ToString(Console.ReadLine());
+if (age>=18)
+{
+    if (cnic == "yes")
+    {
+        Console.WriteLine("Your are eligible");
+    }
+}
---
+++ Day03/Practice05/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter your age");
+int age = Convert.ToInt32(Console.ReadLine());
+if(age is int)
+{
+    Console.WriteLine("Age is Int");
+}
+else
+{
+    Console.WriteLine("Age is not Int");
+}
