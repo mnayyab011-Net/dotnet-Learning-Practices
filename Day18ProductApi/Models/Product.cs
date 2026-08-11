d1ac29a5ddec0a32dd65a66a194f159dc43e1815
mnayyab01-boopb <mnayyab01@gmail.com>
2026-08-11 17:00:21 +0500

    Day 18 - Product API

---
+++ Day18ProductApi/Models/Product.cs
@@ -0,0 +1,8 @@
+namespace Day18ProductApi.Models;
+public class Product
+{
+    public int Id {get;set;}
+    public string Name {get;set;}
+    public decimal Price {get;set;}
+    public String Category {get;set;}
+}
