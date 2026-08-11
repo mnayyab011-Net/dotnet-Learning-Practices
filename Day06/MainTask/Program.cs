6fbf9ad41ac3198690978c548fc617053c540a95
mnayyab01-boopb <mnayyab01@gmail.com>
2026-08-11 16:55:08 +0500

    Day 6 - Inheritance Interfaces and Polymorphism

---
+++ Day06/MainTask/Program.cs
@@ -0,0 +1,60 @@
+﻿interface ICalculateSalary
+{
+    decimal CalculateSalary();
+}
+class Employee
+{
+public string Name;
+    public int Id;
+public Employee(string name, int id)
+    {
+        Name = name;
+        Id = id;
+    }
+public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"ID: {Id}");
+    }
+}
+class Manager : Employee, ICalculateSalary
+{
+    public Manager(string name, int id) : base(name, id)
+    {
+    }
+public decimal CalculateSalary()
+    {
+  return 700000;
+    }
+}class Developer : Employee, ICalculateSalary
+{
+    public Developer(string name, int id) : base(name, id)
+    {
+
+    }
+
+    public decimal CalculateSalary()
+    {
+        return 600000;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Manager manager = new Manager("Ali", 101);
+
+        Console.WriteLine("Manager Details");
+        manager.DisplayInfo();
+        Console.WriteLine($"Salary: {manager.CalculateSalary()}");
+
+        Console.WriteLine();
+
+        Developer developer = new Developer("Ahmed", 102);
+
+        Console.WriteLine("Developer Details");
+        developer.DisplayInfo();
+        Console.WriteLine($"Salary: {developer.CalculateSalary()}");
+    }
+}
---
+++ Day06/Practices01/Program.cs
@@ -0,0 +1,38 @@
+﻿
+interface IAnimal
+{
+    void Sound();
+}
+class Dog : IAnimal
+{
+    public void Sound()
+    {
+        Console.WriteLine("Dog says: Bark");
+    }
+}
+class Cat : IAnimal
+{
+    public void Sound()
+    {
+        Console.WriteLine("Cat says: Meow");
+    }
+}
+class Cow : IAnimal
+{
+    public void Sound()
+    {
+        Console.WriteLine("Cow says: Moo");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog dog = new Dog();
+        dog.Sound();
+        Cat cat = new Cat();
+        cat.Sound();
+        Cow cow = new Cow();
+        cow.Sound();
+    }
+}
