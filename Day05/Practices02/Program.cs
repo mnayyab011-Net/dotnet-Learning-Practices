9051d0cfbc6aee52584b25cb30cef7790e683cae
mnayyab01-boopb <mnayyab01@gmail.com>
2026-08-11 16:54:29 +0500

    Day 5 - OOP Classes and Objects

---
+++ Day05/Practices02/Program.cs
@@ -0,0 +1,17 @@
+﻿Animal animal = new Dog();
+animal.Sound();
+
+class Animal
+{
+    public virtual void Sound()
+    {
+        Console.WriteLine("Animal makes a sound");
+    }
+}
+class Dog : Animal
+{
+    public override void Sound()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
