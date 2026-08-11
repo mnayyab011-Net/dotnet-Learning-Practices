49861e3f8b1a43530bcd6d9b66294e9bd4379709
mnayyab01-boopb <mnayyab01@gmail.com>
2026-08-11 16:56:16 +0500

    Day 8 - Exception Handling

---
+++ Day08/Practices01/Program.cs
@@ -0,0 +1,11 @@
+﻿try
+{
+    Console.WriteLine("Enter your Age");
+    int Age=Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(Age);
+}
+catch (FormatException)
+{
+
+    Console.WriteLine("Please Enter Number Only");
+}
