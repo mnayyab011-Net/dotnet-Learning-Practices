00109b32aa8ebc6168ff500296ab29d7ebaed4aa
mnayyab01-boopb <mnayyab01@gmail.com>
2026-08-11 16:54:14 +0500

    Day 4 - Methods Arrays and Collections

---
+++ Day04/Practices03array+loop/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] sales= new int[6];
+for (int i=0; i<sales.Length; i++)
+{
+    Console.WriteLine("Enter your Sales" + (i+1));
+    sales[i]=Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine();
+int total = 0;
+int max=sales[0];
+int min=sales[0];
+for (int i=0; i<sales.Length; i++)
+{
+    Console.WriteLine("Day: " + (i + 1) + " Sales: " + sales[i]);
+    total=total+sales[i];
+if (sales[i]>max)
+{
+    max=sales[i];
+}
+if (sales[i]<min)
+{
+    min=sales[i];
+}
+}
+Console.WriteLine("Maximum number is:" +max);
+Console.WriteLine("Minimum number is:" +min);
+Console.WriteLine("Total sales is:" +total);
---
+++ Day04/Practices04List/Program.cs
@@ -0,0 +1,21 @@
+﻿List<int> marks = new List<int>();
+for (int i = 0; i < 5; i++)
+{
+Console.Write("Enter Marks " + (i + 1));
+int mark = Convert.ToInt32(Console.ReadLine());
+marks.Add(mark);
+}
+Console.WriteLine();
+int total = 0;
+int max = marks[0];
+for (int i = 0; i < marks.Count; i++)
+{
+Console.WriteLine("Student " + (i + 1) + " Marks  " + marks[i]);
+total = total + marks[i];
+if (marks[i] > max)
+{
+ max = marks[i];
+ }
+}
+Console.WriteLine("Total = " + total);
+Console.WriteLine("max = " + max);
