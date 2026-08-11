535afd3ed7a701c28d1c1130f8f527c67f6372d2
mnayyab01-boopb <mnayyab01@gmail.com>
2026-08-11 16:57:38 +0500

    Day 12 - Async Programming Basics

---
+++ Day12/MainTask/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+class Program
+{
+    static async Task Main()
+    {
+        Console.WriteLine("Start Fetching Data");
+        Task fetchingTask = fetchingDataAsync();
+        Console.WriteLine("Waiting...");
+        await fetchingTask;
+        Console.WriteLine("Data fetch Successfully");
+    }
+    static async Task fetchingDataAsync ()
+    {
+        Console.WriteLine("Data is Feching By System");
+      await Task.Delay(3000);
+    }
+}
---
+++ Day12/Practices02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+class Program
+{
+    static async Task Main()
+    {
+        Console.WriteLine("Press Download Button");
+        Task clickTask = MakeDownloadAsync();
+        Console.WriteLine("Open another website and scroll it.");
+        await clickTask;
+        Console.WriteLine("File download completed.");
+    }
+
+    static async Task MakeDownloadAsync()
+    {
+        Console.WriteLine("Start downloading file...");
+        await Task.Delay(3000);
+        Console.WriteLine("Download completed.");
+    }
+}
